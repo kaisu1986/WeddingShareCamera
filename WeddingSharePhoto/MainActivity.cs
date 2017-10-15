f073230401f83817c0d20ff513285d5b18412225
kaisu <matsubarakei@hotmail.co.jp>
2017-10-15 23:53:35 +0900

    firebase の Database を扱えるように実装中。

--- WeddingSharePhoto/MainActivity.cs
+++ WeddingSharePhoto/MainActivity.cs
@@ -1,12 +1,32 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Firebase.Database;
+using System;
+using Java.Util;
+using Android.Runtime;
 
 namespace WeddingSharePhoto
 {
+    public class UsersValueEventListener : Java.Lang.Object, IValueEventListener
+    {
+        public void OnCancelled(DatabaseError error)
+        {
+        }
+
+        public void OnDataChange(DataSnapshot snapshot)
+        {
+            if (snapshot.Exists() && snapshot.HasChildren)
+            {
+                HashMap dataHashMap = snapshot.Value.JavaCast<HashMap>();
+            }
+        }
+    }
+
     [Activity(Label = "WeddingSharePhoto", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
+        DatabaseReference mDatabase;
         int count = 1;
 
         protected override void OnCreate(Bundle savedInstanceState)
@@ -20,7 +40,15 @@ namespace WeddingSharePhoto
             // and attach an event to it
             Button button = FindViewById<Button>(Resource.Id.myButton);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            button.Click += delegate 
+            {
+                button.Text = string.Format("{0} clicks!", count++);
+            };
+
+            mDatabase = FirebaseDatabase.Instance.Reference;
+
+            var users = mDatabase.Child("users");
+            users.AddValueEventListener(new UsersValueEventListener());
         }
     }
 }
